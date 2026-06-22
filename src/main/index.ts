5172719125877846749c7c34de784fd9bfa19cce
foliageSea <foliageSea@gmail.com>
2026-06-22 23:30:42 +0800

    fix(app): 修复缓存目录访问冲突

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,5 +1,6 @@
-import { app } from 'electron'
+import { app, BrowserWindow } from 'electron'
 import { electronApp } from '@electron-toolkit/utils'
+import { join } from 'path'
 import { createWindow } from './app/createWindow'
 import { registerAppLifecycle, registerWindowAllClosedHandler } from './app/lifecycle'
 import { loadSystemEnvironment } from './app/systemEnvironment'
@@ -7,16 +8,36 @@ import { registerIpc } from './ipc/registerIpc'
 import { registerOpencodeSystemThemeSync } from './opencode/systemTheme'
 
 const appId = 'com.terminus.app'
+const appName = 'terminus'
 
-registerWindowAllClosedHandler()
+app.setName(appName)
+app.setPath('userData', join(app.getPath('appData'), appName))
+app.setPath('sessionData', join(app.getPath('userData'), 'Session Data'))
 
-app.whenReady().then(async () => {
-  await loadSystemEnvironment()
+const gotSingleInstanceLock = app.requestSingleInstanceLock()
 
-  electronApp.setAppUserModelId(appId)
-  registerAppLifecycle()
-  registerIpc()
-  registerOpencodeSystemThemeSync()
+if (!gotSingleInstanceLock) {
+  app.quit()
+} else {
+  app.on('second-instance', () => {
+    const [mainWindow] = BrowserWindow.getAllWindows()
 
-  createWindow()
-})
+    if (!mainWindow) return
+
+    if (mainWindow.isMinimized()) mainWindow.restore()
+    mainWindow.focus()
+  })
+
+  registerWindowAllClosedHandler()
+
+  app.whenReady().then(async () => {
+    await loadSystemEnvironment()
+
+    electronApp.setAppUserModelId(appId)
+    registerAppLifecycle()
+    registerIpc()
+    registerOpencodeSystemThemeSync()
+
+    createWindow()
+  })
+}
